105233cd73c596a2261f17f487d1b29910f5f95c
magmine <aminemag96@gmail.com>
2021-01-14 17:22:03 +0100

    enhanced views

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    #Arguments inside brackets should be the same as the expected argument in the method in the second argument of path
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
+from django.http import Http404
+#from django.template import loader
+
+from .models import Question
 
 '''
 The view that manages the Urls 
 '''
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:3]
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+    #return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question, 'question_id': question_id})
+
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    response = "<h2>You're looking at the results of question %s.</h2>"
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("<h3>You're voting on question %s.</h3>" % question_id)
