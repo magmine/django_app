cac17fb956614a6f137a15cd20190ed5921f7012
magmine <aminemag96@gmail.com>
2021-01-22 17:00:08 +0100

    add results.html template

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 #from django.template import loader
 
 from .models import Question, Choice
@@ -55,3 +56,19 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+
+#     def get_queryset(self):
+#         return Question.objects.order_by('-pub_date')[:3]
+
+# class DetailView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/detail.html'
+
+# class ResultsView(generic.DetailView):
+#     model = Question
+#     template_name = 'polls/results.html'
+
